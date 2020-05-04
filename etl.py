7987b84d6374b4e3654dd8649714281d58863a97
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-04 19:27:13 +0530

    Main method in etl code

--- etl.py
+++ etl.py
@@ -4,3 +4,15 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
