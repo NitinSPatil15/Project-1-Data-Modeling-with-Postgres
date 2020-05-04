66e2fa28d7fb24377a7d99cce98f6a8058907059
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-04 19:12:04 +0530

    Drop Tables queries

---
+++ sql_queries.py
@@ -0,0 +1,7 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
