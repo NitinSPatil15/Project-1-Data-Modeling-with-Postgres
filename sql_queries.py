6cd7dd1cc64319dc166e5099f3f1fe07af3aef0b
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-04 19:17:29 +0530

    Query lists queries

--- sql_queries.py
+++ sql_queries.py
@@ -113,3 +113,8 @@ VALUES
 %s, %s, %s, %s, %s, %s, %s
 )
 """)
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
