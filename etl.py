fa46d8a1fd148a5b53946bb8121a78c5989558ec
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-04 19:30:01 +0530

    Process a song file in etl code

--- etl.py
+++ etl.py
@@ -4,6 +4,29 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+
+"""
+    This procedure processes a song file whose filepath has been provided as an arugment.
+    It extracts the song information in order to store it into the songs table.
+    Then it extracts the artist information in order to store it into the artists table.
+
+    INPUTS:
+    * cur the cursor variable
+    * filepath the file path to the song file
+    """
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
+    cur.execute(artist_table_insert, artist_data)
+
+
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
