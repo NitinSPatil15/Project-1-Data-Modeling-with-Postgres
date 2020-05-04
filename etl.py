ef3cbb6156a4b4b9e084a0b85c91ae26f73f31d4
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-04 19:35:10 +0530

    Process a log file in etl code

--- etl.py
+++ etl.py
@@ -27,6 +27,61 @@ def process_song_file(cur, filepath):
     cur.execute(artist_table_insert, artist_data)
 
 
+"""
+    This procedure processes a log file whose filepath has been provided as an arugment.
+    It extracts the song start time information, tansforms it and then store it into the time table.
+    Then it extracts the users information in order to store it into the users table.
+    Finally it extrats informations from songs table, artists table and original log file to store it into the songplays table.
+
+    INPUTS:
+    * cur the cursor variable
+    * filepath the file path to the song file
+    """
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df['page']=='NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'], unit='ms')
+    
+    # insert time data records
+    time_data = (df['ts'].values.tolist(), t.dt.hour.values.tolist(), 
+             t.dt.day.values.tolist(), t.dt.week.values.tolist(), 
+             t.dt.month.values.tolist(), t.dt.year.values.tolist(), 
+             t.dt.weekday.values.tolist(), )
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
